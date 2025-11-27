7a4a672d3d5117a8cdd52999a39e87a39272d973
atirathtradersindia <info@atirathtraders.com>
2025-11-27 13:09:02 +0530

    Update main.py

--- siea-home/sai-basmati-website/backend/main.py
+++ siea-home/sai-basmati-website/backend/main.py
@@ -457,4 +457,4 @@ async def create_order(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
